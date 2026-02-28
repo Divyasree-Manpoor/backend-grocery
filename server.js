88ba4a0691cc0790039690e5d62a60154cab7eff
manpoordivyasree26-glitch <manpoordivyasree26@gmail.com>
2026-02-28 18:34:24 +0530

    Added

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ testDatabaseConnection();
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9392;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
